d8e9634fa3c08a3750713905a0585a644bcfca06
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-06-08 16:40:08 +0100

    ..

---
+++ portfolio-aggregator-dapp/src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Portfolio Radar - A web3 data aggregator dapp.",
+  description:
+    "Portfolio Radar (Port-Radar) is a Decentralized Application (DApp) that enables users to aggregate web3 data in auser friendly & modern UI",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
