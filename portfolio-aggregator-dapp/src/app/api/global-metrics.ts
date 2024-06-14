eeb1e5b421c9679690fe3cfc28acf5d5b9d6339e
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-06-14 11:22:35 +0100

    Update global-metrics.ts

--- portfolio-aggregator-dapp/src/app/api/global-metrics.ts
+++ portfolio-aggregator-dapp/src/app/api/global-metrics.ts
@@ -1,48 +1,48 @@
-import React from "react";
-import axios from "axios";
-import { url } from "inspector";
-import { headers } from "next/headers";
+// import React from "react";
+// import axios from "axios";
+// import { url } from "inspector";
+// import { headers } from "next/headers";
 
-const apiKey = process.env.COINMARKETCAP_API_KEY;
+// const apiKey = process.env.COINMARKETCAP_API_KEY;
 
-export const getStaticProps = async () => {
-  const config = {
-    url: "https://pro-api.coinmarketcap.com/v1/global-metrics/quotes/latest",
-    headers: {
-      "X-CMC_PRO_API_KEY": apiKey,
-    },
-  };
+// export const getStaticProps = async () => {
+//   const config = {
+//     url: "https://pro-api.coinmarketcap.com/v1/global-metrics/quotes/latest",
+//     headers: {
+//       "X-CMC_PRO_API_KEY": apiKey,
+//     },
+//   };
 
-  try {
-    const response = await axios.request(config);
-    const data = response.data;
+//   try {
+//     const response = await axios.request(config);
+//     const data = response.data;
 
-    const {
-      data: {
-        active_cryptocurrencies,
-        btc_dominance,
-        eth_dominance,
-        quote: {
-          USD: { total_market_cap, total_volume_24h },
-        },
-      },
-    } = data;
+//     const {
+//       data: {
+//         active_cryptocurrencies,
+//         btc_dominance,
+//         eth_dominance,
+//         quote: {
+//           USD: { total_market_cap, total_volume_24h },
+//         },
+//       },
+//     } = data;
 
-    console.log(data);
+//     console.log(data);
 
-    return {
-      props: {
-        totalCryptocurrencies: active_cryptocurrencies,
-        btcdominance: btc_dominance,
-        ethdominance: eth_dominance,
-        totalMarketCap: total_market_cap,
-        totalVolume24h: total_volume_24h,
-      },
-    };
-  } catch (error) {
-    console.log("error fetching data", error);
-    return {
-      props: {},
-    };
-  }
-};
+//     return {
+//       props: {
+//         totalCryptocurrencies: active_cryptocurrencies,
+//         btcdominance: btc_dominance,
+//         ethdominance: eth_dominance,
+//         totalMarketCap: total_market_cap,
+//         totalVolume24h: total_volume_24h,
+//       },
+//     };
+//   } catch (error) {
+//     console.log("error fetching data", error);
+//     return {
+//       props: {},
+//     };
+//   }
+// };
