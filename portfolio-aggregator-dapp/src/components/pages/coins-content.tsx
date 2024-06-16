a3eaffd720654b641a5cc3ab24f06f45e27f2da5
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-06-16 20:33:16 +0100

    Update coins-content.tsx

--- portfolio-aggregator-dapp/src/components/pages/coins-content.tsx
+++ portfolio-aggregator-dapp/src/components/pages/coins-content.tsx
@@ -1,14 +1,8 @@
-import Link from "next/link";
-import Image from "next/image";
-
 import { Card, CardContent } from "@/components/ui/card";
 import CryptoPriceTable from "@/features/table/crypto-price-table";
 import GlobalMarketsMetricsMenubar from "@/features/markets-infinite-menubar";
-// import { getGlobalMetrics } from "@/app/api/get-global-metrics";
 
 export default async function CoinsContent() {
-  // const data = await getGlobalMetrics();
-
   return (
     <Card className="rounded-lg border-none mt-6">
       <CardContent className="p-6">
