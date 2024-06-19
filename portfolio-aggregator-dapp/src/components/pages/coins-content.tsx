ce257d9c3fb40d2598be83c891458f708c34ec37
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-06-19 09:45:10 +0100

    Update coins-content.tsx

--- portfolio-aggregator-dapp/src/components/pages/coins-content.tsx
+++ portfolio-aggregator-dapp/src/components/pages/coins-content.tsx
@@ -2,21 +2,12 @@ import { Card, CardContent } from "@/components/ui/card";
 import CryptoPriceTable from "@/features/table/crypto-price-table";
 import GlobalMarketsMetricsMenubar from "@/features/markets-infinite-menubar";
 
-export default async function CoinsContent() {
+export default function CoinsContent() {
   return (
     <Card className="rounded-lg border-none mt-6">
       <CardContent className="p-6">
         <GlobalMarketsMetricsMenubar />
         <div className="flex flex-row justify-center items-center min-h-[calc(100vh-56px-64px-20px-24px-56px-48px)]">
-          {/* <div className="flex mt-5 flex- relative">
-            <Image
-              src={Oxose}
-              alt="Placeholder Image"
-              width={500}
-              height={500}
-              priority
-            />
-          </div> */}
           <CryptoPriceTable />
         </div>
       </CardContent>
